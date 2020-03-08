a4a3fbab3d138ecd60993c613882dc1d5007c420
Naveen Ithapu <nithapu@gainsight.com>
2020-03-08 16:11:43 +0530

    some changesa

--- timeline/messages.js
+++ timeline/messages.js
@@ -1,4 +1,4 @@
-export const HELLO = "Hellow World";
+export const HELLOW = "Hellow World";
 export const Name = "Hellow World";
 export const Name2 = "Hellow World";
 export const Name3 = "Hellow World";
